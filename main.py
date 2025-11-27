c2da792edc54e059ede92399a4e91cd65114f358
_cyberDilf <vlad.aks03@mail.ru>
2025-11-27 18:11:41 +0500

    added pillow

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from PIL import Image
+
+
 def main() -> None:
     print("Hello, World!")
+
     total = 1 + 1
     print(total)
+
+    image = Image.open("img.png")
+    print(image.size)
 
 
 if __name__ == '__main__':
